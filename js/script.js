4055d4cffcab757676495073a45561bca57a52bd
Lukas Mettler <mettler.lukas@bluewin.ch>
2019-12-04 17:01:21 +0100

    add ajax function

--- js/script.js
+++ js/script.js
@@ -80,3 +80,13 @@ function animate(context, x, y) {
 /*
 Get request with Ajax
 */
+function getServerDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "ajax-get-info.txt", true);
+  xhttp.send();
+}
